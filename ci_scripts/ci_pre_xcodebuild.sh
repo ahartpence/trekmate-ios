f62cbae7c75e7ed7f82e7f09a0945435b3afa614
Andrew Hartpence <andrew.hartpence@gmail.com>
2024-10-29 07:36:46 -0400

    add empty secrets.json

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Stage: PRE-Xcode Build is activated .... "
-echo $(ls)
+#echo $(ls)
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
@@ -9,7 +9,7 @@ echo $(ls)
 
 # Write a JSON File containing all the environment variables and secrets.
 printf "{\"API_KEY\":\"%s\"\"}" "$API_KEY">> ../TrekMate/secrets.json
-cat ../TrekMate/secrets.json
+#cat ../TrekMate/secrets.json
 echo "Wrote Secrets.json file."
 
 echo "Stage: PRE-Xcode Build is DONE .... "
