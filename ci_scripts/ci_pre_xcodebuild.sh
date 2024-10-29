7de69d942d815b6d4e2dafbf885483aa17fa34f5
Andrew Hartpence <andrew.hartpence@gmail.com>
2024-10-29 06:58:03 -0400

    debug script

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 echo "Stage: PRE-Xcode Build is activated .... "
+echo $(ls)
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_WORKSPACE/ci_scripts || exit 1
+#cd $CI_WORKSPACE/ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
 printf "{\"API_KEY\":\"%s\"\"}" "$API_KEY">> ../TrekMate/secrets.json
